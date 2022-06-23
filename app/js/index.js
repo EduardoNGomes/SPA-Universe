7780b0dd10894d14cf4a97a8582f553c00676f11
Eduardo N Gomes <Eduardo.000@live.com>
2022-06-23 16:43:07 -0300

    resolved bug

--- app/js/index.js
+++ app/js/index.js
@@ -2,9 +2,9 @@ import { Router } from './router.js'
 
 const router = new Router()
 
-router.add('/', '../app/pages/home.html')
-router.add('/universe', '../app/pages/universe.html')
-router.add('/explorer', '../app/pages/explorer.html')
+router.add('/', './app/pages/home.html')
+router.add('/universe', './app/pages/universe.html')
+router.add('/explorer', './app/pages/explorer.html')
 router.add(404, './app/pages/404.html')
 
 router.handle()
